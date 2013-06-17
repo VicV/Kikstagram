6d9e651d35c2afd70c08181f567ec16762c4c05b
Vic Vu <victorvuc@gmail.com>
2013-06-17 13:34:28 -0400

    caching

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-web: zerver  -p web
-=======
-web: zerver -p web
->>>>>>> df9ce2d9242d331af865e691ef47257a5e461cfa
+web: zerver -p --manifest=cache.manifest web
--- web/js/search.js
+++ web/js/search.js
@@ -3,6 +3,9 @@ App.populator('search', function (page) {
 	lastPage = 0;
 	p.find('.app-button.search').on('click', function(){
 		var input = p.find('.app-input.tag').val().toLowerCase();
+		var str = "This is a sentence"; 
+		var matches = input.split(/\b/);
+		input = matches.length > 0 ? matches[0] : "";
 		var data = { searchInput : input };
 		if (input == null || input.length == 0 || input == "" ) {
 			App.dialog({title: "Empty search", text: "Please enter a tag"})
@@ -24,3 +27,7 @@ App.populator('search', function (page) {
 		}
 	}
 );
+
+ZERVER_TIMEOUT = 15000;
+
+
